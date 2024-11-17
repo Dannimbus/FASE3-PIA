dadec5c4f25f1d5a60faf5f7288fd4287ca253e3
Emm-ily <159584218+Emm-ily@users.noreply.github.com>
2024-11-17 14:43:34 -0600

    Add files via upload

--- payment.js
+++ payment.js
@@ -1,30 +1,13 @@
-/* Obtener parámetros para la funcion */
-function getQueryParams(){
-    const params={};
-    const urlParams = new URLSearchParams(window.location.search);
-    params.name = urlParams.get('name');
-    params.price = urlParams.get('price')
-    params.image = urlParams.get('image')
-    return params;
+function buyProduct(id, name, image, description, price) {
+    const product = { id, name, image, description, price };
+    localStorage.setItem('selectedProduct', JSON.stringify(product)); // guarda solo un procudto
+    window.location.href = 'payment.html'; // Redirige a la página de pago
+  }
+  
+ 
+
+
+function continuarPago() {
+    alert("Procediendo con el pago...");
+    // agregar pantalla para procesar el pago
 }
-
-//llenar pantalla con datos de produtos
-const productInfo = getQueryParams();
-document.getElementById('product-info').innerHTML = `
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="${productInfo.image}" class="img-fluid rounded-start" alt="${productInfo.name}">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">${productInfo.name}</h5>
-                <p class="card-text">Precio: $${productInfo.price}</p>
-                <p class="card-text">Descripción breve del producto aquí.</p>
-            </div>
-        </div>
-    </div>
-`;
-
-document.getElementById('add-to-cart').addEventListener('click', () => {
-    alert(`${productInfo.name} ha sido agregado al carrito.`);
-});
